880f4174e708c1b9cad2561760d3d958490bc887
Johannes <johannes@grimstadgiske.no>
2025-02-01 23:18:39 +0100

    fix: adjust monster speed calculation for higher waves

--- src/util/getMonsterSpeed.ts
+++ src/util/getMonsterSpeed.ts
@@ -3,5 +3,8 @@ export const getMonsterSpeed = (wave: number) => {
   if (wave > 2) {
     speed += (wave - 2) * 0.1;
   }
-  return Math.min(speed, 5);
+  if (wave > 40) {
+    speed += (wave - 40) * 0.1;
+  }
+  return Math.min(speed, 10);
 };
